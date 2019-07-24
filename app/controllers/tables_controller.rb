af211d168d9b4ef6ecf88a07a9fd2d06fe0bedde
donkey96 <noire964@gmail.com>
2019-07-24 19:29:51 +0900

    add table create function

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -1,11 +1,24 @@
 class TablesController < ApplicationController
 
   def index
-    @tables = Table.all
   end
 
   def new
     @table = Table.new
+  end
+
+  def create
+    @table = Table.new(table_params)
+    if @table.save
+      redirect_to root_path, notice: "テーブルを登録しました"
+    else
+      render :new
+    end
+  end
+
+  private
+  def table_params
+    params.require(:table).permit(:name).merge(user_id: current_user.id)
   end
 
 end
