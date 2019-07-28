47365b3f4e808d092c56a1951f9b2f192d7be51d
donkey96 <noire964@gmail.com>
2019-07-28 18:23:57 +0900

    add  incremental search

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,60 @@
+$(function () {
+
+var search_list = $(".customers");
+
+function appendCustomer(customer) {
+  var html = `<div class="customer">
+                <div class="customer_name">
+                  ${ customer.name } 様
+                </div>
+                <div class="customer_people">
+                  ${ customer.people } 名
+                </div>
+                <div class="customer_tel">
+                  ${ customer.tel }
+                </div>
+                <div class="customer_day">
+                  ${ customer.day }
+                </div>
+                <div class="customer_time">
+                  ${ customer.time }
+                </div>
+                <div class="customer_text">
+                  ${ customer.text }
+                </div>
+                <input type="button" value="追加" class="customer_add-btn">
+              </div>`
+  search_list.append(html);
+}
+function appendErrMsgToHTML(msg) {
+  var html = `<div class="customer">
+                ${ msg }
+              </div>`
+  search_list.append(html);
+}
+  $('.customer-search').on('keyup', function(){
+    var input = $('.customer-search').val();
+
+    $.ajax({
+      type: 'GET',
+      url: '/customers',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+
+    .done(function (customers) {
+      $(".customers").empty();
+      if (customers.length !== 0) {
+        customers.forEach(function (customer) {
+          appendCustomer(customer);
+        });
+      }
+      else {
+        appendErrMsgToHTML("一致する顧客情報はありません");
+      }
+    })
+    .fail(function () {
+      alert('検索に失敗しました')
+    });
+  }); 
+});
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_26_015601) do
+ActiveRecord::Schema.define(version: 2019_07_27_081221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2019_07_26_015601) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "day"
+    t.index ["day"], name: "index_customers_on_day"
+    t.index ["tel"], name: "index_customers_on_tel"
     t.index ["user_id"], name: "index_customers_on_user_id"
   end
 
