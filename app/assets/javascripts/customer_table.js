241a4de2778cd3c51d1fff25101f162aa1821c8b
donkey96 <noire964@gmail.com>
2019-08-07 09:58:13 +0900

    add-customer-update

--- app/assets/javascripts/customer_table.js
+++ app/assets/javascripts/customer_table.js
@@ -1,5 +1,10 @@
 $(document).on('turbolinks:load',function() {
+var cookie = $.cookie('table');
+if(cookie){
+  $(this).toggleClass("lightpink");
+}
   $(".table").on("click", function() {
     $(this).toggleClass("lightpink");
+    $.cookie("table","lightpink");
   });
 });
