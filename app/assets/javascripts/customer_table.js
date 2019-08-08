17ce2a93f6d6f1d0ea226647cc181e31327502f6
donkey96 <noire964@gmail.com>
2019-08-08 15:13:22 +0900

    update customer index

--- app/assets/javascripts/customer_table.js
+++ app/assets/javascripts/customer_table.js
@@ -1,10 +1,10 @@
 $(document).on('turbolinks:load',function() {
-var cookie = $.cookie('table');
-if(cookie){
-  $(this).toggleClass("lightpink");
-}
+// var cookie = $.cookie('table');
+// if(cookie){
+//   $(".table").toggleClass("lightpink");
+// }
   $(".table").on("click", function() {
     $(this).toggleClass("lightpink");
-    $.cookie("table","lightpink");
+    // $.cookie("table","lightpink");
   });
 });
