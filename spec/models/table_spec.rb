4d7f93c520a5fc4feddb537680d2a2f876245637
donkey96 <noire96@gmail.com>
2020-04-27 19:30:04 +0900

    テーブルモデルのテスト

---
+++ spec/models/table_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+describe Table do
+  describe '#create' do
+    it 'テーブル名がない場合登録できない' do
+      table = Table.new(name: "")
+      table.valid?
+      expect(table.errors[:name]).to include("を入力してください")
+    end
+  end
+end
