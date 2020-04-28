30f5268a0cb5cb742d230a0e02088ad02363e43e
donkey96 <noire96@gmail.com>
2020-04-28 20:38:38 +0900

    テーブルのテストにFactoryBotを利用

--- spec/models/table_spec.rb
+++ spec/models/table_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 describe Table do
   describe '#create' do
     it 'テーブル名がない場合登録できない' do
-      table = Table.new(name: "")
+      table = build(:table, name: "")
       table.valid?
       expect(table.errors[:name]).to include("を入力してください")
     end
