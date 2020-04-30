b21fc6c2962418c65f1f89878354b0db1e4eb3c6
donkey96 <noire96@gmail.com>
2020-04-30 21:55:33 +0900

    GET #newのテスト

--- spec/controllers/tables_controller_spec.rb
+++ spec/controllers/tables_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-describe TablesController do
-  
+describe TablesController, type: :controller do
+  describe 'GET #new' do
+    it "new.html.hamlに遷移すること" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
 end
