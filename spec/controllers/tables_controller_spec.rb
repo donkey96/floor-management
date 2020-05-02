7c72e4077cfa339e1772c006aa45198697a8d325
donkey96 <noire96@gmail.com>
2020-05-02 18:40:06 +0900

    #newのログイン状態の検証

--- spec/controllers/tables_controller_spec.rb
+++ spec/controllers/tables_controller_spec.rb
@@ -1,10 +1,26 @@
 require 'rails_helper'
 
 describe TablesController, type: :controller do
+  let(:user) { create(:user) }
+  let(:table) { create(:table) }
+
   describe 'GET #new' do
-    it "new.html.hamlに遷移すること" do
-      get :new
-      expect(response).to render_template :new
+    context 'ログインしている場合' do
+      before do
+        login user
+        get :new
+      end
+      it "new.html.hamlに遷移すること" do
+        expect(response).to render_template :new
+      end
     end
-  end
+    context 'ログインしていない場合' do
+      before do
+        get :new
+      end
+      it "ログイン画面にリダイレクトすること" do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end  
 end
