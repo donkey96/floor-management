9e7ddd4b38728504ddd3dd71561f6e5d0b65a46b
donkey96 <noire96@gmail.com>
2020-05-03 13:34:21 +0900

    ベースの作成

---
+++ spec/controllers/customers_controller_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe CustomersController, type: :controller do
+  let(:user) {create(:user)}
+  let(:customer) {create(:customer)}
+  let(:table) {create(:table)}
+
+end
