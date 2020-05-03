31a192217661366a58ce604dc81eaef33d21208a
donkey96 <noire96@gmail.com>
2020-05-03 13:49:41 +0900

    大枠作り

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -1,8 +1,31 @@
 require 'rails_helper'
 
 describe CustomersController, type: :controller do
-  let(:user) {create(:user)}
-  let(:customer) {create(:customer)}
-  let(:table) {create(:table)}
+  let(:user) { create(:user) }
+  let(:customer) { create(:customer) }
+  let(:table) { create(:table) }
 
+  describe 'GET #new' do
+
+    context 'ログインしている場合' do
+      before do
+
+      end
+
+      it 'new.html.hamlに遷移すること' do
+
+      end
+    end
+
+    context 'ログインしていない場合' do
+      before do
+
+      end
+
+      it "ログイン画面にリダイレクトすること" do
+
+      end
+    end
+  end
+  
 end
