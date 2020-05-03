fe78046b01f79a498679a7b635bf268838a2ea01
donkey96 <noire96@gmail.com>
2020-05-03 13:55:54 +0900

    ログイン処理の作成

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -9,7 +9,8 @@ describe CustomersController, type: :controller do
 
     context 'ログインしている場合' do
       before do
-
+        login user
+        get :new
       end
 
       it 'new.html.hamlに遷移すること' do
@@ -19,11 +20,11 @@ describe CustomersController, type: :controller do
 
     context 'ログインしていない場合' do
       before do
-
+        get :new
       end
 
       it "ログイン画面にリダイレクトすること" do
-
+      expect(response).to redirect_to(new_user_session_path)
       end
     end
   end
