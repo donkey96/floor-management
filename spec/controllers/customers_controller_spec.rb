1f39816270714e131b314ed2c48d30356d377b2c
donkey96 <noire96@gmail.com>
2020-05-05 20:30:12 +0900

    ログイン処理の記載

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -54,6 +54,9 @@ describe CustomersController, type: :controller do
   describe "POST #create" do
 
     context "ログインしている場合" do
+      before do
+      login user
+      end
   
       context "保存に成功した場合" do
 
@@ -79,9 +82,10 @@ describe CustomersController, type: :controller do
     end
     
     context "ログインしていない場合" do
-
-      it "ログイン画面にリダイレクトすること" do
       
+      it "ログイン画面にリダイレクトすること" do
+        post :create
+        expect(response).to redirect_to(new_user_session_path)
       end
     end
   end
