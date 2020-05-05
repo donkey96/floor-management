4504e0248e1beb0e198cc98764bb427bc9835b7b
donkey96 <noire96@gmail.com>
2020-05-05 21:27:35 +0900

    必須項目作成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
   has_many :customers_tables
   has_many :tables, through: :customers_tables
+
+  validates :name, presence: true
+  validates :tel, presence: true
 end
