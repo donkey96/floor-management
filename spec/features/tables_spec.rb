3f3a39f1adf4f7589f69b2a10a8796e336a4fa24
donkey96 <noire96@gmail.com>
2020-05-08 20:31:42 +0900

    未ログイン時の確認

--- spec/features/tables_spec.rb
+++ spec/features/tables_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 feature 'table', type: :feature do
+  let(:user) { create(:user) }
 
+  scenario 'テーブルを作成できること' do
+    # 未ログイン時はログイン画面に遷移する
+    visit root_path
+    expect(current_path).to eq new_user_session_path
+  end
 end
