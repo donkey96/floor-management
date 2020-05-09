8eb266f0b138e2358286e9a84de4c1b5c0dfbf27
donkey96 <noire96@gmail.com>
2020-05-09 21:26:12 +0900

    予約登録

--- spec/features/customer_spec.rb
+++ spec/features/customer_spec.rb
@@ -16,5 +16,19 @@ feature 'table', type: :feature do
     fill_in 'user_password', with: user.password
     find('input[name="commit"]').click
     expect(current_path).to eq customers_path
+
+    # 顧客登録
+    expect {
+      click_link('当日予約')
+      expect(current_path).to eq new_customer_path
+      fill_in 'customer_name', with: 'aaa'
+      fill_in 'customer_people', with: '10'
+      fill_in 'customer_tel', with: '12345678910'
+      fill_in 'customer_day', with: '12345678'
+      fill_in 'customer_time', with: '1234'
+      fill_in 'customer_text', with: 'aaaaaaaaaaaaaaaa'
+      find('input[type="submit"]').click
+    }.to change(Customer, :count).by(1)
+    expect(current_path).to eq root_path
   end
 end
