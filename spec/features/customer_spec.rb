b0cfc0fba1f6002845873204903d511e136fc957
donkey96 <noire96@gmail.com>
2020-05-09 21:08:57 +0900

    ログイン処理

--- spec/features/customer_spec.rb
+++ spec/features/customer_spec.rb
@@ -9,5 +9,12 @@ feature 'table', type: :feature do
     # 未ログイン時はログイン画面に遷移
     visit customers_path
     expect(current_path).to eq new_user_session_path
+
+    # ログイン処理
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    find('input[name="commit"]').click
+    expect(current_path).to eq customers_path
   end
 end
