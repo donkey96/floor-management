c8376d2a2400f0c1260484ab590486298d0f3cd3
donkey96 <noire96@gmail.com>
2020-05-09 21:05:02 +0900

    未ログイン時のチェック

--- spec/features/customer_spec.rb
+++ spec/features/customer_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 feature 'table', type: :feature do
+  let(:user) { create(:user) }
+  let(:table) { create(:table) }
+  let(:customer) { create(:customer) }
 
+  scenario 'customer情報を更新できる' do
+    # 未ログイン時はログイン画面に遷移
+    visit customers_path
+    expect(current_path).to eq new_user_session_path
+  end
 end
